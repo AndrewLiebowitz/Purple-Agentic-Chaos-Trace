721a80bc237bcf3b654900f82a1907eb6638b65e
Andrew Liebowitz <aliebowitz@google.com>
2026-01-21 16:39:46 -0500

    fix: correct terraform syntax error

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,13 @@
-variable "project_id" { type = string }
-variable "region" { type = string; default = "us-central1" }
-variable "repo_name" { type = string; default = "agents-repo" }
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "repo_name" {
+  type    = string
+  default = "agents-repo"
+}
